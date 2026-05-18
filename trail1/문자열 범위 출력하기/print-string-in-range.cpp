0d4f20a9e85ea59eb62ed2c99aa0cae45ade8608
김태훈 <132832057+taehun2123@users.noreply.github.com>
2026-05-18 11:01:55 +0900

    [Passed] 문자열 범위 출력하기 (8ms, 0MB)

---
+++ trail1/문자열 범위 출력하기/print-string-in-range.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    string str;
+    getline(cin, str);
+    for(int i = 2; i<10; i++){
+        cout << str[i];
+    }
+    return 0;
+}
