63f8af6592caae7223c90a672b12756359ee8244
김태훈 <132832057+taehun2123@users.noreply.github.com>
2026-05-18 11:41:14 +0900

    [Passed] 단어로 구분하기 2 (8ms, 0MB)

---
+++ trail1/단어로 구분하기 2/separate-words-with-words-2.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+string str[10];
+int main() {
+    for(int i = 0; i<10; i++) cin >> str[i];
+    for(int i = 0; i<10; i++){
+        if(i%2==0) cout << str[i] << "\n";
+    }
+    return 0;
+}
