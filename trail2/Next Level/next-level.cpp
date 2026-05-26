74f50a44b9265ef2dec89928304765432f08ff8c
김태훈 <132832057+taehun2123@users.noreply.github.com>
2026-05-26 19:35:04 +0900

    [Passed] Next Level (2ms, 0MB)

---
+++ trail2/Next Level/next-level.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+string str = "codetree";
+int level = 10;
+int main() {
+    cout << "user " << str << " lv " << level << "\n"; 
+    cin >> str >> level;
+    cout << "user " << str << " lv " << level << "\n"; 
+    return 0;
+}
